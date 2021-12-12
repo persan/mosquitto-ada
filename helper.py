d006d0bba18f6b1045025c8080fabe2d6f490011
persan <per.s.sandberg@bahnhof.se>
2021-12-12 08:59:03 +0100

    Get workflow to run on github

--- helper.py
+++ helper.py
@@ -6,5 +6,5 @@ if exists(sys.argv[1]):
     if "bin/" in sys.argv[1]:
         print (subprocess.check_output(sys.argv[1]))
     else:
-        with file(sys.argv[1]) as inf:
+        with open(sys.argv[1]) as inf:
             print (inf.readline().strip())
