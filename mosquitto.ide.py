d44e6fa33c5b1894b593af53424ae1edd62dce8d
Per Sandberg <per.s.sandberg@bahnhof.se>
2023-08-03 17:39:16 +0200

    Prepare for release

--- mosquitto.ide.py
+++ mosquitto.ide.py
@@ -1,1 +1,14 @@
-mosquitto_ide.py
+import GPS
+import autoformat
+from os.path import *
+
+
+def initialize_project_plugin():
+    if exists("case_exceptions.xml"):
+        with open("case_exceptions.xml") as inf:
+            GPS.parse_xml(inf.read())
+
+
+def finalize_project_plugin():
+    pass
+
