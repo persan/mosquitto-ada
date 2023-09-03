6647d4cb9d1545d77342e7995b4a7ad0f3f02828
Per Sandberg <per.s.sandberg@bahnhof.se>
2023-09-03 07:27:14 +0200

    Update with link dependencies

---
+++ tests/mosquitto-tests.ide.py
@@ -0,0 +1,16 @@
+import GPS
+from os.path import *
+
+def case_exceptions() -> []:
+    return ["../case_exceptions.xml"]
+
+
+def initialize_project_plugin():
+    for i in case_exceptions():
+        if exists(i):
+            with open(i) as inf:
+                GPS.parse_xml(inf.read())
+
+
+def finalize_project_plugin():
+    pass
